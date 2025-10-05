5f3798e2b5f45482e14db36a7a62e7f3eb1c428a
Christoph Wulf <christoph.wulf@talentformation.com>
2025-10-05 21:35:59 +0200

    Improve TypeScript and Svelte configuration
    * Use more defaults
    * Improve pre-rendering

--- template/{{cookiecutter.project_slug}}/frontend/svelte.config.js
+++ template/{{cookiecutter.project_slug}}/frontend/svelte.config.js
@@ -4,7 +4,22 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ fallback: 'dynamic.html' }) }
+	kit: {
+		adapter: adapter({
+			fallback: 'fallback.html'
+		}),
+		prerender: {
+			handleHttpError: ({ status, path, referrer, message }) => {
+				if (status === 404) {
+					// ignore 404s discovered during crawl
+					console.log(`Ignoring not found for ${path} from ${referrer} with ${status} ${message}`);
+					return;
+				}
+				// rethrow others
+				throw new Error(`Prerender failed for ${path} from ${referrer} with ${status} ${message}`);
+			}
+		}
+	}
 };
 
 export default config;
